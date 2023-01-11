961aedf4ea50c984861a12abf0e1cdc950ab10af
이민기 <96801658+Reefel@users.noreply.github.com>
2023-01-11 14:50:18 +0900

    Update a better way.cs

--- 0001-two-sum/a better way.cs
+++ 0001-two-sum/a better way.cs
@@ -1,14 +1,19 @@
-int numsLength = nums.Length;
-Dictionary<int, int> dic = new Dictionary<int, int>();
-int [ ] answer = new int [ 2 ];
-for ( int i = 0; i < numsLength; i++ )
-{
-    if(dic.ContainsKey(target - nums[i]))
+class Soulution {
+    public int[] twoSum(int[] nums, int target)
     {
-        answer [ 1 ] = i;
-        answer [ 0 ] = dic.FirstOrDefault(a => a.Key == ( target - nums [ i ] )).Value;
+        int numsLength = nums.Length;
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+        int [ ] answer = new int [ 2 ];
+        for ( int i = 0; i < numsLength; i++ )
+        {
+            if(dic.ContainsKey(target - nums[i]))
+            {
+                answer [ 1 ] = i;
+                answer [ 0 ] = dic.FirstOrDefault(a => a.Key == ( target - nums [ i ] )).Value;
+                return answer;
+            }
+            dic.Add(nums [ i ], i);
+        }
         return answer;
     }
-    dic.Add(nums [ i ], i);
 }
-return answer;
