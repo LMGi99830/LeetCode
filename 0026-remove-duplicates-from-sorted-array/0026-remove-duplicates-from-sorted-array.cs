6dbf40ee3c405bb3b6ef7ad9cfc23f263b1c3f7f
이민기 <96801658+Reefel@users.noreply.github.com>
2023-01-30 14:52:04 +0900

    Time: 154 ms (67.73%), Space: 46.8 MB (6.25%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -4,13 +4,7 @@ public class Solution {
         for ( int i = 0; i < expectAnswer.Length; i++ )
         {
             nums [ i ] = expectAnswer [ i ];
-            if (i == expectAnswer.Length - 1)
-            {
-                List<int> list = nums.ToList();
-                list.RemoveRange(i + 1, nums.Length - expectAnswer.Length);
-                nums = list.ToArray();
-            }
         }
-        return nums.Length;
+        return expectAnswer.Length;
     }
 }
