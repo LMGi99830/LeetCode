641dde3ec0e40f719b7f7a857f13f2b914b50791
이민기 <96801658+Reefel@users.noreply.github.com>
2023-01-30 11:12:38 +0900

    Update 0021-merge-two-sorted-lists.cs

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cs
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cs
@@ -11,6 +11,8 @@
  */
 public class Solution {
     public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+        
+        //둘 중 하나가 null이면 그 반대의 값을 return 시켜준다.
         if(l1 == null) return l2;
         if (l2 == null) return l1;
 
@@ -19,6 +21,8 @@ public class Solution {
 
         while(l1 != null && l2 != null)
         {
+            //오름차순 정렬을 위해 작은 값을 curr.next에 넣어주고 l1 or l2를 다음 값으로 증가 시킨다.
+            //curr에 curr의 다음값을 넣는다.
             if(l1.val <= l2.val)
             {
                 curr.next = l1;
